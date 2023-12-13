f88fe2102b235027416c64b1160183f4daf13c67
ACER <ACER@DESKTOP-3LUM61J>
2023-12-13 23:27:53 +0300

    Add project files.

---
+++ TabloYedekleme/Form1.cs
@@ -0,0 +1,84 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TabloYedekleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connectionString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillTableList();
+        }
+        private void FillTableList()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SHOW TABLES";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        checkedListBox1.Items.Add(reader.GetString(0));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata oluştu: " + ex.Message);
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            List<string> liste = new List<string>();
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                liste.Add(item.ToString());
+            }
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if(folder.ShowDialog() == DialogResult.OK)
+            {
+                RestoreAndBackup.BackupTable(connectionString, liste, folder.SelectedPath);
+                MessageBox.Show("Seçilen Tablolarda Yedek Alındı!");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, false);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "SQL FİLE |*.sql";
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                string SafeFileName = Path.GetFileNameWithoutExtension(openFile.SafeFileName);
+                RestoreAndBackup.RestoreTable(connectionString, SafeFileName, openFile.FileName);
+                MessageBox.Show(SafeFileName + " Yüklendi");
+            }
+        }
+    }
+}
---
+++ TabloYedekleme/TableRestore.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using static System.Windows.Forms.CheckedListBox;
+
+namespace TabloYedekleme
+{
+    public static class RestoreAndBackup
+    {
+        public static void BackupTable(string connectionString, List<string> tableName, string backupPath)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    string yol = $"{backupPath}\\{DateTime.Today.ToShortDateString()} backup.sql";
+                    using (MySqlBackup mb = new MySqlBackup(cmd))
+                    {
+                        cmd.Connection = connection;
+                        mb.ExportInfo.TablesToBeExportedList = tableName;
+                        mb.ExportToFile(yol);
+                    }
+                }
+            }
+        }
+        public static void RestoreTable(string connectionString, string tableName, string backupPath)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        using (MySqlBackup mb = new MySqlBackup(cmd))
+                        {
+                            cmd.Connection = connection;
+                            mb.ExportInfo.TablesToBeExportedList = new List<string>()
+                            {
+                                tableName
+                            };
+                            mb.ImportFromFile(backupPath);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
